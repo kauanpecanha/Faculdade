cd0d3e965c0f00c03d8e6de84ab8cb3652e04142
Kauan Peçanha <kauanpecanha1@gmail.com>
2023-10-30 22:44:01 -0300

    checkpoint

--- modlin/multiple_regression/index.py
+++ modlin/multiple_regression/index.py
@@ -6,7 +6,7 @@ x = np.array([
     [1, 16, 5],
     [1, 29, 3],
     [1, 19, 0],
-    [1, 21, 2],
+    [1, 27, 2],
 ])
 
 y = np.array([
@@ -21,80 +21,76 @@ x_transpose = np.transpose(x)
 
 prodx = np.dot(x_transpose, x)
 
-# print(prodx)
-
 prodx_inv = np.linalg.inv(prodx)
 
-# print(prodx_inv)
+prod = np.dot(prodx_inv, x_transpose)
 
-xty = np.dot(x_transpose, y)
+print(prod) # igual ao do professor
 
-# print(xty)
+beta = np.dot(prod, y)
 
-beta = np.dot(prodx_inv, xty)
+print(beta) # diferente do professor
 
-# print(beta)
-
-print(f"Equação do MRLM: {beta[0][0]:.2f} + {beta[1][0]:.2f} * x1 + {beta[2][0]:.2f} * x2")
+# print(f"Equação do MRLM: {beta[0][0]:.2f} + {beta[1][0]:.2f} * x1 + {beta[2][0]:.2f} * x2")
 
 
-# montagem da tabela anova
-n = 5
-p = 2
-coef = 5.65
-# coef = float(input("Entre com o coeficiente: "))
+# # montagem da tabela anova
+# n = 5
+# p = 2
+# coef = 5.65
+# # coef = float(input("Entre com o coeficiente: "))
 
-y_med = np.mean(y)
+# y_med = np.mean(y)
 
-y_transpose = np.transpose(y)
+# y_transpose = np.transpose(y)
 
-sqt = ( np.dot(y_transpose, y) ) - ( n * (y_med**2) )
+# sqt = ( np.dot(y_transpose, y) ) - ( n * (y_med**2) )
 
-beta_transpose = (np.transpose(beta))
+# beta_transpose = (np.transpose(beta))
 
-sqreg = ( np.dot(np.dot(beta_transpose, x_transpose), y)) - ( n * (y_med**2) )
+# sqreg = ( np.dot(np.dot(beta_transpose, x_transpose), y)) - ( n * (y_med**2) )
 
-sqe = sqt - sqreg
+# sqe = sqt - sqreg
 
-# print(sqt[0][0])
-# print(sqreg[0][0])
-# print(sqe[0][0])
+# # print(sqt[0][0])
+# # print(sqreg[0][0])
+# # print(sqe[0][0])
 
-sqreg = float(sqreg[0][0])
-sqt = float(sqt[0][0])
-sqe = float(sqe[0][0])
+# sqreg = float(sqreg[0][0])
+# sqt = float(sqt[0][0])
+# sqe = float(sqe[0][0])
 
-gl = np.array([p, n-p-1, n-1])
-sq = np.array([sqreg, sqe, sqt])
-qm = np.array([(sqreg/p), (sqe/n-p-1), 0])
-f0 = np.array([((sqreg/p)/(sqe/n-p-1)), 0, 0])
-
-
-# for i in range(len(gl)):
-    # print(f"{gl[i]:.2f} \t {sq[i]:.2f} \t {qm[i]:.2f} \t {f0[i]:.2f}")
-
-if f0[0] > coef:
-    print(f"Hipótese H0 rejeitada(modelo de regressão linear múltipla)")
-else:
-    print(f"Hipótese H1 rejeitada(modelo simples)")
+# gl = np.array([p, n-p-1, n-1])
+# sq = np.array([sqreg, sqe, sqt])
+# qm = np.array([(sqreg/p), (sqe/n-p-1), 0])
+# f0 = np.array([((sqreg/p)/(sqe/n-p-1)), 0, 0])
 
 
-# Estatística de Teste de Hipótese
+# # for i in range(len(gl)):
+#     # print(f"{gl[i]:.2f} \t {sq[i]:.2f} \t {qm[i]:.2f} \t {f0[i]:.2f}")
 
-tau_square = (np.dot((np.transpose((y - (np.dot(x, beta))))), (y - (np.dot(x, beta))))/n - p - 1)
-tau_square = tau_square[0][0]
-
-comparisson_constant = 4.21
-
-# print(tau_square)
-# print(prodx_inv)
+# if f0[0] > coef:
+#     print(f"Hipótese H0 rejeitada(modelo de regressão linear múltipla)")
+# else:
+#     print(f"Hipótese H1 rejeitada(modelo simples)")
 
 
-for k in range(0, len(beta)):
-    print(k)
-    t_bk = beta[k]/sqrt(tau_square * prodx_inv[k][k])
+# # Estatística de Teste de Hipótese
+
+# tau_square = (np.dot((np.transpose((y - (np.dot(x, beta))))), (y - (np.dot(x, beta))))/n - p - 1)
+# tau_square = tau_square[0][0]
+
+# comparisson_constant = 4.21
+
+# # print(tau_square)
+# # print(prodx_inv)
+
+
+# for k in range(0, len(beta)):
+#     print(k)
+#     t_bk = beta[k]/sqrt(tau_square * prodx_inv[k][k])
     
-    if t_bk**2 >= comparisson_constant:
-        print(f"Para {beta[k]}, a hipótese nula é rejeitada.\nValor de t_bk**2 = {t_bk**2}\n")
-    else:
-        print(f"Para {beta[k]}, a hipótese não-nula é rejeitada.\nValor de t_bk**2 = {t_bk**2}\n")
+#     if t_bk**2 >= comparisson_constant:
+#         print(f"Para {beta[k]}, a hipótese nula é rejeitada.\nValor de t_bk**2 = {t_bk**2}\n")
+#     else:
+#         print(f"Para {beta[k]}, a hipótese não-nula é rejeitada.\nValor de t_bk**2 = {t_bk**2}\n")
